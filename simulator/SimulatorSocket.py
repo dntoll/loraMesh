36eca0111e8422a1aba39c7b94a555311f7ab4ac
Daniel <daniel.toll@lnu.se>
2022-02-13 09:47:01 +0100

    gitignore

--- simulator/SimulatorSocket.py
+++ simulator/SimulatorSocket.py
@@ -1,19 +1,22 @@
 
-globalRadioBuffer = "";
-
 class SimulatorSocket:
 	
 	
 	def __init__(self, mac):
 		self.mac = mac
+		self.sendBuffer = bytearray(b"")
+		self.receiveBuffer = bytearray(b"")
         
 	def send(self, bytes):
-		globalRadioBuffer += bytes
+		
+		for i in bytes:
+			self.sendBuffer.append(i)
 		return
         
 	def receive(self):
-		global globalRadioBuffer 
-		return (globalRadioBuffer, {})
+		rec = bytes(self.receiveBuffer)
+		self.receiveBuffer = bytearray(b"")
+		return (rec, {"rssi" : -50})
 
 	def getMac(self):
 		return self.mac
